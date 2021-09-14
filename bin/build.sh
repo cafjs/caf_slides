705fbd4ff0b25c258f8daa0bc29a9126fad006d3
Antonio Lain <antlai@cafjs.com>
2021-09-13 20:12:05 -0700

    Fix CDN
    Signed-off-by: Antonio Lain <antlai@cafjs.com>

--- bin/build.sh
+++ bin/build.sh
@@ -9,7 +9,24 @@ export NODE_ENV=production
 browserify js/main.js | uglifyjs > js/build.min.js &
 pid2=$!
 unset NODE_ENV
-popd
+
+#build user view
+pushd user
+echo "browserify  -d js/main.js -o js/build.js"
+browserify  -d js/main.js -o js/build.js &
+pid3=$!
+
+echo "browserify js/main.js | uglifyjs > js/build.min.js"
+export NODE_ENV=production
+browserify js/main.js | uglifyjs > js/build.min.js &
+pid4=$!
+
+unset NODE_ENV
+popd #user
+popd #public
+
 
 wait $pid1
 wait $pid2
+wait $pid3
+wait $pid4
